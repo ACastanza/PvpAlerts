3ac73f3b6b3caac281b872c4078bb710b5c16421
Cullen Daugherty <dakjaniels@outlook.com>
2025-02-24 06:04:56 -0600

    Fix battleground score retrieval by passing correct team index

--- PvpAlerts/PvpAlerts_Battleground_Scoreboard.lua
+++ PvpAlerts/PvpAlerts_Battleground_Scoreboard.lua
@@ -718,7 +718,7 @@ function ScoreboardList:FilterScrollList()
 			local scores = {}
 			local nTeams = GetBattlegroundNumTeams(battlegroundId)
 			for i = 1, nTeams do
-				table.insert(scores, { GetCurrentBattlegroundScore(GetCurrentBattlegroundRoundIndex()), i })
+				table.insert(scores, { GetCurrentBattlegroundScore(i), i })
 			end
 
 			local function sortingFn(score1, score2)
